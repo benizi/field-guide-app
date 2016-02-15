d31a83459d587545f61e0365f0245416a6cfa486
Benjamin R. Haskell <elixir@benizi.com>
2016-02-14 23:04:28 -0500

    Fix app name from hello world example

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,7 @@ config :field_guide, FieldGuide.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Heroku DB config
-config :hello_phoenix, HelloPhoenix.Repo,
+config :field_guide, FieldGuide.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20
